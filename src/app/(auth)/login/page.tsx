feb745e8d683bdcd3582e91ff7dfeeba2b0499f4
luthfiabdllh <ahmadabdllh000@gmail.com>
2025-02-01 23:38:06 +0700

    deploy

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { LoginForm } from "@/components/login-form"
-import { Button } from "@/components/ui/button"
-import { signIn } from "next-auth/react"
 
 export default function Login() {
   return (
